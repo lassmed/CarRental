2bb1bc68ad98133bafceabe12f8bb00d1e3b4cb1
lassmed <mohamed.lasswed2001@gmail.com>
2024-09-19 17:40:21 +0100

    final commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using CarRentalSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using CarRentalSystem.Repository; 
+
+namespace CarRentalSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IData data;
+
+        
+        public HomeController(ILogger<HomeController> logger , IData data)
+        {
+            _logger = logger;
+            this.data = data; 
+        }
+        public IActionResult Index()
+        {
+            var list = data.GetAllCars();
+            return View(list);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+        }
+    }
+}
---
+++ Repository/IData.cs
@@ -0,0 +1,17 @@
+﻿using CarRentalSystem.Models;
+namespace CarRentalSystem.Repository
+{
+	public interface IData
+	{
+        bool AddNewCar(Car newcar);
+        public List<Car> GetAllCars();
+
+        bool AddDriver(Driver newdriver);
+        public List<Driver> GetAllDrivers();
+
+        public bool BookingNow(Rent rent);
+        //List<string> GetBrand(); 
+
+
+    }
+}
